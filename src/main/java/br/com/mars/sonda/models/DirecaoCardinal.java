c6bbc34ebe1eca51f352b518f0277dc418ff0215
ThiagoAndrad <thpandrade@gmail.com>
2018-01-26 23:19:23 -0200

    [Regitrar Sonda] criando atributos de posicionamento da sonda assim como a direcao cardinal usando como layout a rosa dos ventos

---
+++ src/main/java/br/com/mars/sonda/models/DirecaoCardinal.java
@@ -0,0 +1,8 @@
+package br.com.mars.sonda.models;
+
+public enum DirecaoCardinal {
+    N,
+    E,
+    S,
+    W
+}
--- src/main/java/br/com/mars/sonda/viewModel/SondaViewModel.java
+++ src/main/java/br/com/mars/sonda/viewModel/SondaViewModel.java
@@ -1,4 +1,13 @@
 package br.com.mars.sonda.viewModel;
 
+import br.com.mars.sonda.models.DirecaoCardinal;
+
 public class SondaViewModel {
+
+    private DirecaoCardinal direcaoCardinal;
+
+    private int eixoX;
+
+    private int eixoY;
+
 }
