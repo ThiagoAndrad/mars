80ffbb51cf224e2114749df7eaf08ee32e760dec
ThiagoAndrad <thpandrade@gmail.com>
2018-01-30 20:22:14 -0200

    [Direcionar Sonda] criando assinaturas descrevendo o cenario para direcionar a sonda

---
+++ src/test/java/br/com/mars/sonda/models/SondaTest.java
@@ -0,0 +1,16 @@
+package br.com.mars.sonda.models;
+
+import org.junit.Test;
+
+public class SondaTest {
+
+    @Test
+    public void aoVirarADireitaPosicionaASondaParaADirecaoConformeARosaDosVentos() {
+
+    }
+
+    @Test
+    public void aoVirarAEsquerdaPosicionaASondaConformeARosaDosVentos() {
+
+    }
+}
